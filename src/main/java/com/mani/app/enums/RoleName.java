37ce24526e5ebf79ff65067a025a59bf61a6d664
tmani007 <iammanikumar97@gmail.com>
2023-02-04 20:43:50 +0530

    first commit

---
+++ src/main/java/com/mani/app/enums/RoleName.java
@@ -0,0 +1,6 @@
+package com.mani.app.enums;
+
+public enum RoleName {
+    ROLE_ADMIN,
+    ROLE_USER;
+}
---
+++ src/main/java/com/mani/app/repositories/ParkingSpotRepository.java
@@ -0,0 +1,13 @@
+package com.mani.app.repositories;
+
+import com.mani.app.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, Long> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+}
